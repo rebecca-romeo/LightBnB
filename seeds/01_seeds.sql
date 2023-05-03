ca979ff021a70a1cca6a7b202647566de7cd0ea1
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-03 13:48:51 -0400

    add 3 rows of data to each table

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,26 @@
+-- USERS
+INSERT INTO users (name, email, password)
+VALUES
+('Betty Blep', 'betty@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+-- -- PROPERTIES
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', 'true' ),
+(2, 'Blank corner', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 530.61, 5, 2, 6, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83142', 'true' ),
+(3, 'Blank corner', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 720.90, 6, 3, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland', '83142', 'true' );
+
+-- RESERVATIONS
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2023-10-01', '2023-10-14', 3, 3);
+
+-- PROPERTY REVIEWS
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (1, 1, 1, 3, 'message here'),
+(2, 2, 2, 4, 'messages here'),
+(3, 3, 3, 4, 'messages here');
+
+
